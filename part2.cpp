9cb5a68a1f5f477aa2c4675216c1b1f2ed003041
jonathan <jonathanbedoy1@gmail.com>
2018-02-21 15:11:47 -0800

    added part2 w/ comments

--- part2.cpp
+++ part2.cpp
@@ -22,8 +22,12 @@ int main(void)
 		//read in the line of input
 		getline(cin, a[0]);
 		int found[3];
+        //if the string reads history or ! then we do the following
+        //it is in a while loop just in case they continue to type "history" 
+        //or the enter a number out of range
         while (a[0] == "history" || a[0][0] == '!')
         {
+            //if history then shows last 10 commands entered with corresponding number
             if(a[0] == "history")
             {
                 for(int i = 0;i< 10;i++)
@@ -31,8 +35,10 @@ int main(void)
                     cout << history-i << ": " << hist[i] << endl;
                 }
             }
+            //if ! then the following
             if(a[0][0] == '!')
             {
+                //if !! then repeats the previous command
                 if(a[0][1] == '!')
                 {
                     if(history1 != 0)
@@ -41,6 +47,7 @@ int main(void)
                         a[0] = hist[9];
                     break;
                 }
+                //else reads in the number and repeats the command corresponfing to that number
                 else
                 {
                     a[0].erase(0,1);
@@ -52,18 +59,15 @@ int main(void)
                     }
                 }
             }
+            //we only end up here if the user types history or if the user entered a number that is our of range
             printf("osh>");
             getline(cin, a[0]);
         }
-        /*if(a[0][0] == '!')
-        {
-           
-        }
-        */
-        
+        //before the string is parsed, it gets stored into history
         hist[history1] = a[0];
-        cout << hist[history1] << " " << history1;
+        //cout << hist[history1] << " " << history1;
         history++;
+        //history1 acts as an index for history so when it reaches 9 it has to be reset to 0
         if(history1 < 9)
         {
             history1++;
